355ebbab96dba1babbf760f8978106a116db5887
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-03 15:25:18 -0300

    mocks of info and icons added

---
+++ src/mocks/data.ts
@@ -0,0 +1,122 @@
+
+import linkedin from "../assets/icons/brand-linkedin.svg";
+import github from "../assets/icons/brand-github.svg";
+import gmail from "../assets/icons/brand-gmail.svg";
+
+import react from "../assets/icons/brand-react.svg";
+import java from "../assets/icons/java-svgrepo-com.svg";
+import nodejs from "../assets/icons/brand-nodejs.svg";
+import tailwind from "../assets/icons/brand-tailwind.svg";
+import typescript from "../assets/icons/brand-typescript.svg";
+import javascript from "../assets/icons/brand-javascript.svg";
+import html5 from "../assets/icons/file-type-html.svg";
+import css3 from "../assets/icons/brand-css3.svg";
+import spring from "../assets/icons/spring-boot-svgrepo-com.svg";
+import mysql from "../assets/icons/brand-mysql.svg";
+export const socialMediaData = [
+  {
+    id: 1,
+    name: "Linkedin",
+    icon: linkedin,
+    url: "https://www.linkedin.com/in/tadeowoto/",
+  },
+  {
+    id: 2,
+    name: "Github",
+    icon: github,
+    url: "https://github.com/tadeowoto",
+  },
+  {
+    id: 3,
+    name: "Gmail",
+    icon: gmail,
+    url: "mailto:tadeo.woto@gmail.com",
+  }
+];
+
+export const skillsData = [
+  {
+    id: 1,
+    name: "React",
+    icon: react,
+  },
+  {
+    id: 2,
+    name: "Java",
+    icon: java,
+  },
+  {
+    id: 3,
+    name: "Nodejs",
+    icon: nodejs,
+  },
+  {
+    id: 4,
+    name: "Tailwind",
+    icon: tailwind,
+  },
+  {
+    id: 5,
+    name: "Typescript",
+    icon: typescript,
+  },
+  {
+    id: 6,
+    name: "Javascript",
+    icon: javascript,
+  },
+  {
+    id: 7,
+    name: "HTML",
+    icon: html5,
+  },
+  {
+    id: 8,
+    name: "CSS",
+    icon: css3,
+  },
+  {
+    id: 9,
+    name: "MySql",
+    icon: mysql,
+  },
+  {
+    id: 10,
+    name: "Spring Boot",
+    icon: spring,
+  }
+];
+
+
+/*
+export const imagesData = [
+  {
+    id: 1,
+    src: "/project1.png",
+    title: "React retro ecoommerce",
+    tags: ["React", "Tailwind", "Typescript"],
+    to: "https://retro-ecommerce-jade.vercel.app",
+  },
+  {
+    id: 2,
+    src: "/project2.png",
+    title: "Mission Control X",
+    tags: ["React", "Tailwind", "Typescript"],
+    to: "https://mission-controlx.vercel.app",
+  },
+  {
+    id: 3,
+    src: "/project3.png",
+    title: "Artify",
+    tags: ["html", "css", "javascript"],
+    to: "https://artify-topaz.vercel.app",
+  },
+  {
+    id: 4,
+    src: "/project4.png",
+    title: "Windows 95 TODO",
+    tags: ["React", "Tailwind", "Typescript"],
+    to: "https://windows95-todo.vercel.app/",
+  },
+];
+*/
